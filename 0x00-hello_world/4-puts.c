10b38d0ac7d59b641ec3554e1fd667aef4315eea
Okafor Chukwunonso Joseph <okaforchukwunonsojoseph2019@gmail.com>
2022-09-08 18:31:18 -0700

    4-puts.c_corrected_2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * A programm that prints out the dtring 'Programming is like building a multil
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
