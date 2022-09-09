04b6f30e9b69e585bce32516029a76a3a4700b4e
Okafor Chukwunonso Joseph <okaforchukwunonsojoseph2019@gmail.com>
2022-09-08 19:09:06 -0700

    4-puts.c_corrected_5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * A programm that prints out a string of characters.
- */
-int main(void)
+* A programm that prints out a string of characters.
+*/
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
