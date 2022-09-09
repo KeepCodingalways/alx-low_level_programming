bd6fd9d3b41111df5cdc5b85927320cfdc9cab0e
Okafor Chukwunonso Joseph <okaforchukwunonsojoseph2019@gmail.com>
2022-09-08 18:45:40 -0700

    4-puts.c_corrected_4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * A programm that prints out the dtring 'Programming is like building a multil
+ * A programm that prints out a string of characters.
  */
 int main(void)
 {
